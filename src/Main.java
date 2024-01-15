7beffcadfd7abce5cccfb456d399c80538d774cc
Xavier Favier <xaxou13@gmail.com>
2024-01-15 11:42:04 +0100

    Exercice 2A 2. Fibonacci

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Vector;
+
 public class Main {
     //2A.1
     static void enumerate_while(int max) {
@@ -18,9 +20,9 @@ public class Main {
     }
     static void enumerate_even(int max) {
         //3.
-        for(int a=0; a < max; a++) {
-            if((a+1) % 2 == 0) {
-                System.out.println(a+1);
+        for(int a=0; a < max+1; a++) {
+            if(a%2 == 0) {
+                System.out.println(a);
             }
         }
     }
@@ -49,6 +51,53 @@ public class Main {
         }
     }
 
+    static int[] fibo(int x) {
+        Vector<Integer> myVect = new Vector<Integer>();
+
+        if(x > 0) {
+            myVect.add(0);
+        }
+        if(x > 1) {
+            myVect.add(1);
+        }
+
+        for(int i=2; i < x; i++) {
+            myVect.add(myVect.getLast() + myVect.get(myVect.size()-2));
+        }
+
+        int[] myArr = new int[myVect.size()];
+        for(int i=0; i < myVect.size(); i++) {
+            myArr[i] = myVect.get(i);
+        }
+
+        return myArr;
+    }
+
+    static double[] approximation_fibo(double x) {
+        double a=2;
+        double b=1;
+        double c=1;
+
+        int i=0;
+        while(i < 100) {
+            if(Math.abs(a/b-1.6180333) <= x) {
+                double[] ans = new double[2];
+                ans[0] = a;
+                ans[1] = b;
+                return ans;
+            } else {
+                c=b;
+                b=a;
+                a=b+c;
+            }
+            i++;
+        }
+        double[] ans = new double[2];
+        return ans;
+    }
+
+
+
     public static void main(String[] args) {
         //2A.1
         //enumerate_while(100);
@@ -57,6 +106,17 @@ public class Main {
 
         //2A.2
         //System.out.println(small_square(30));
-        System.out.println(big_square(63));
+        //System.out.println(big_square(63));
+
+        //int[] myArr = fibo(10);
+        //for(int i=0; i < myArr.length; i++) {
+        //    System.out.println(myArr[i]);
+        //}
+
+        double[] myArr = approximation_fibo(0.01);
+        System.out.println("here");
+        for(int i=0; i < myArr.length; i++) {
+            System.out.println(myArr[i]);
+        }
     }
 }
